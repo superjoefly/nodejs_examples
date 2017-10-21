98d8e8e20ba9f56aefcc118e49d5d0bdc104fa2b
superjoefly <superjoefly@gmail.com>
2017-10-21 17:10:10 -0600

    Updated files and added notes and snippets for File-System Module.

--- scripts/app.js
+++ scripts/app.js
@@ -35,18 +35,124 @@
 //////////////////////
 
 
-// Splitting the Query String
-var http = require('http');
-var url = require('url');
+// // Splitting the Query String
+// var http = require('http');
+// var url = require('url');
+//
+// http.createServer(function(req, res) {
+//   // content type
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   // parse the query
+//   var q = url.parse(req.url, true).query;
+//   // split the query
+//   var txt = q.year + " " + q.month;
+//   // end response
+//   res.end(txt);
+//   // listen on port 8080
+// }).listen(8080);
 
-http.createServer(function(req, res) {
-  // content type
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  // parse the query
-  var q = url.parse(req.url, true).query;
-  // split the query
-  var txt = q.year + " " + q.month;
-  // end response
-  res.end(txt);
-  // listen on port 8080
-}).listen(8080);
+
+//////////////////////
+
+
+// Reading Files
+
+
+// // Read HTML File
+// var http = require('http');
+// var fs = require('fs');
+//
+// http.createServer(function(req, res) {
+//   // Read the file
+//   fs.readFile('../demos/demoFile1.html', function(err, data) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     // Write the data
+//     res.write(data);
+//     res.end();
+//   });
+// }).listen(8080);
+
+
+///////////////////
+
+
+// Creating Files //
+
+
+// // Create a New File using appendFile() method
+// var fs = require('fs');
+//
+// fs.appendFile('../demos/newFile1.txt', 'New File Content!', function(err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
+
+
+// // Create a New File using open() method
+// var fs = require('fs');
+//
+// fs.open('../demos/newFile2.txt', 'w', function(err, file) {
+//   if (err) throw err;
+//   console.log('New File Saved!');
+// });
+
+
+// // Create a New File using writeFile() method
+// var fs = require('fs');
+//
+// fs.writeFile('../demos/newFile3.txt', 'New File Content!', function(err) {
+//   if (err) throw err;
+//   console.log('New File Saved!');
+// });
+
+
+////////////////////
+
+
+// Updating Files //
+
+
+// // Update Existing File using appendFile() method
+// var fs = require('fs');
+//
+// fs.appendFile('../demos/newFile1.txt', ' This is my new text!', function(err) {
+//   if (err) throw err;
+//   console.log('File Updated!');
+// });
+
+
+// // Replace Existing File using writeFile() method
+// var fs = require('fs');
+//
+// fs.writeFile('../demos/newFile3.txt', 'This is a new, updated file!', function(err) {
+//   if (err) throw err;
+//   console.log('File Replaced and Updated!');
+// });
+
+
+////////////////////
+
+
+// Deleting Files //
+
+// // Delete File
+// var fs = require('fs');
+//
+// fs.unlink('../demos/newFile2.txt', function(err) {
+//   if (err) throw err;
+//   console.log('File Deleted!');
+// });
+
+
+///////////////////
+
+// Renaming Files
+
+// Rename Existing File using the rename() method
+
+var fs = require('fs');
+
+fs.rename('../demos/fileToRename.txt', '../demos/renamedFile.txt', function(err) {
+  if (err) throw err;
+  console.log('File Renamed!');
+});
